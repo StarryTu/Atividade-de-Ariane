d4c50d5c16ddcda8aac98d96d6e0199eb2ffe7d9
StarryTu <barbalhoa78@gmail.com>
2026-06-25 13:46:35 -0300

    Exclusão de classes inúteis

--- src/CriarArquivos.java
+++ src/CriarArquivos.java
@@ -1,3 +1,21 @@
-public class CriarArquivos {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 
+public class CriarArquivos {
+    public static void main(String[] args) {
+        Path caminho = Paths.get("ArquivoPraTeste.txt"); //O nome do arquivo 
+        //bloco de try pra fazer o arquivo
+        try {
+            //Criação do arquivo em si
+            Files.createFile(caminho);
+        }
+        catch(IOException e){
+            //Captura do erro de entrada/saida de dados e mostra na tela o erro
+            System.err.println("O erro: " + e.getMessage());
+        }
+
+    }
+    
 }
--- src/EscreverDados.java
+++ src/EscreverDados.java
@@ -1,3 +1,20 @@
-public class EscreverDados {
-
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+public class EscreverDados {  
+    public static void main(String[] args) {
+        //Um buffered reader pra entrada de dados digitado pelo usuário
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter("ArquivoPraTeste.txt"))){
+            System.out.print("Digite o que vai ter no arquivo: ");
+            String digitado = br.readLine();
+            bw.write(digitado);
+            System.out.print("Texto digitado no arquivo, pode conferir ele");
+        } catch(IOException e){
+            System.out.print("Erro na entrada de dados");
+        }
+    }
+    
 }
